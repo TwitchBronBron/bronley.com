e6d91e974f039b176e9ced7761654c009bf85fd9
Bronley Plumb <bronley@gmail.com>
2018-08-23 23:15:12 -0400

    Enhanced the comment system.

--- assets/post.js
+++ assets/post.js
@@ -1,13 +1,32 @@
 function showReplyForm(element) {
+    //hide all nested reply forms
+    resetVisibility();
+
+    //remove all comment nesting when adding a comment 
+    document.body.classList.add('nested-reply-active');
+
+    //hide the "reply" anchor
     element.classList.add('hide');
+
+    //show the form
     var form = element.nextElementSibling;
     form.classList.remove('hide');
+    form.getElementsByClassName('name-input')[0].focus();
 }
 
-function cancelReply(element){
+function cancelReply(element) {
+    //show the reply anchor
     element.parentElement.parentElement.getElementsByClassName('comment-reply-anchor')[0].classList.remove('hide');
+
+    //hide the form
     var form = element.parentElement;
     form.classList.add('hide');
+
+    //remove all comment nesting when adding a comment 
+    document.body.classList.remove('nested-reply-active');
+
+    //hide all nested reply forms
+    resetVisibility();
 }
 
 if (document.readyState !== 'loading') {
@@ -21,11 +40,18 @@ if (document.readyState !== 'loading') {
 
 //progressive enhancement
 function init() {
+    resetVisibility();
+}
+
+function resetVisibility() {
     //hide all of the reply forms
     var elements = document.getElementsByClassName('add-comment-form');
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
-
+        //skip the root comment form
+        if (element.classList.contains('root-comment-form')) {
+            continue;
+        }
         element.classList.add('hide');
     }
 
